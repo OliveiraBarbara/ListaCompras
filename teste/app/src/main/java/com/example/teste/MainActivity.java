3189e85ed5112dd44a7296befcda2d3be2712192
Barbara Oliveira <barbara_oliveira@ufms.br>
2022-10-06 19:47:54 -0400

    Mudando Tela

--- teste/app/src/main/java/com/example/teste/MainActivity.java
+++ teste/app/src/main/java/com/example/teste/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.teste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bAddItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.bAddItem = findViewById(R.id.bAddItem1);
+
+        this.bAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Inserir.class);
+                startActivity(i);
+            }
+        });
     }
 }
